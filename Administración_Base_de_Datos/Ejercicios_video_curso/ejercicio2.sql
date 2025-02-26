05ff52cc5acb43463d1cb0d425699cfe566db0e8
joseGS10 <josua75@hotmail.es>
2025-02-26 20:42:33 +0100

    Update ejercicio2.sql

--- Administración_Base_de_Datos/Ejercicios_video_curso/ejercicio2.sql
+++ Administración_Base_de_Datos/Ejercicios_video_curso/ejercicio2.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Ventas (
   id_venta INT PRIMARY KEY AUTO_INCREMENT,
   fecha_venta DATETIME,,
-  monto_total DECIMAL(12,2),
+  monto_total DECIMAL(8,2),
   metodo_pago VARCHAR(20),
-  id_cliente INT FOREIGN KEY REFERENCES ,
+  id_cliente INT,
+  FOREIGN KEY (id_cliente)  REFERENCES  Clientes(id_cliente)
 );
